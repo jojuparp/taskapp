e2c050195a2ce4a0bd9666a4ff1c638860ae2753
Joni <parpala.joni@gmail.com>
2022-05-03 10:25:18 +0300

    remove unnecessary resources

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App(): JSX.Element {
+
+  const handleClick: React.MouseEventHandler = () => console.log('hmm')
+  const div = <div onClick={handleClick}></div>
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
       </header>
     </div>
   );
